fae8f51762d62bed025516dc5b29148a3d8644cc
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:13:13 +0400

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -1,1 +1,10 @@
+from pyrogram import filters
+from bot import app
 
+@app.on_message(filters.command("mirror"))
+async def mirror(_, message):
+    if len(message.command) == 1:
+        return await message.reply("❌ Send a link to mirror")
+
+    link = message.text.split(maxsplit=1)[1]
+    await message.reply(f"📥 Mirroring started:\n{link}")
