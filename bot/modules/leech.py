8c4848987074dc7c5799fc80c90d74ba48a67129
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:13:37 +0400

    Update leech.py

--- bot/modules/leech.py
+++ bot/modules/leech.py
@@ -1,1 +1,10 @@
+from pyrogram import filters
+from bot import app
 
+@app.on_message(filters.command("leech"))
+async def leech(_, message):
+    if len(message.command) == 1:
+        return await message.reply("❌ Send a link to leech")
+
+    link = message.text.split(maxsplit=1)[1]
+    await message.reply(f"📤 Leech started:\n{link}")
