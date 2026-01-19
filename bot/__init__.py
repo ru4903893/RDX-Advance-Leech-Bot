447ee6282e808ae136d8fdf23964c634a7971a36
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:15:10 +0400

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,11 @@
+from pyrogram import Client
+from bot.config import Config
 
+app = Client(
+    "adv-kpsml",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.TELEGRAM_API,
+    api_hash=Config.TELEGRAM_HASH,
+    workers=50,
+    in_memory=True
+)
