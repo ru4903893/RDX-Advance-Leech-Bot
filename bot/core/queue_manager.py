3e4b995ab09ef91d7d52ecd3e67e63399524d30d
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:23:13 +0400

    Update queue_manager.py

--- bot/core/queue_manager.py
+++ bot/core/queue_manager.py
@@ -1,1 +1,14 @@
+import asyncio
 
+QUEUE = []
+MAX_QUEUE = 3
+
+async def add_queue(task):
+    QUEUE.append(task)
+
+async def process_queue():
+    while True:
+        if QUEUE:
+            task = QUEUE.pop(0)
+            await task()
+        await asyncio.sleep(1)
