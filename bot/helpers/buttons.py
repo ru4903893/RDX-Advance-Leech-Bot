d1aa303ce278dc12dafd5fbac5e63b86d98c0703
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:44:19 +0400

    Update buttons.py

--- bot/helpers/buttons.py
+++ bot/helpers/buttons.py
@@ -1,1 +1,8 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def start_buttons():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("📢 Channel", url="https://t.me/your_channel")],
+        [InlineKeyboardButton("👤 Owner", url="https://t.me/your_username")],
+        [InlineKeyboardButton("⚙ User Settings", callback_data="usetting")]
+    ])
