14f09e1851222dad2c0c3e0347a30640cd3239a0
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:26:18 +0400

    Update leech.py

--- bot/modules/leech.py
+++ bot/modules/leech.py
@@ -1,10 +1,20 @@
 from pyrogram import filters
 from bot import app
+from bot.database.tasks_db import add_task
+import uuid
 
 @app.on_message(filters.command("leech"))
 async def leech(_, message):
     if len(message.command) == 1:
-        return await message.reply("❌ Send a link to leech")
+        return await message.reply("❌ Send a link")
 
     link = message.text.split(maxsplit=1)[1]
-    await message.reply(f"📤 Leech started:\n{link}")
+    gid = str(uuid.uuid4())[:8]
+
+    await add_task(gid, message.from_user.id, "Leech Task", "Downloading")
+
+    await message.reply(
+        f"📤 **Leech Started**\n"
+        f"🆔 `{gid}`\n"
+        f"🔗 {link}"
+    )
