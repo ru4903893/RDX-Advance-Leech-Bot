f1a17da708a325c6ca7e28f6e52875ae191ca993
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:10:24 +0400

    Update mongodb.py

--- bot/database/mongodb.py
+++ bot/database/mongodb.py
@@ -1,1 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from bot.config import Config
 
+db = None
+
+async def init_db():
+    global db
+    client = AsyncIOMotorClient(Config.DATABASE_URL)
+    db = client["RDX"]
+    print("✅ MongoDB Connected")
