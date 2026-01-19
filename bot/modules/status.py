bbe704110322f47c6f38f789a0df82dd34026e0d
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:16:03 +0400

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -1,1 +1,11 @@
+from pyrogram import filters
+from bot import app
+from bot.database.tasks_db import get_tasks
 
+@app.on_message(filters.command("status"))
+async def status(_, message):
+    text = "📊 **Active Tasks**\n\n"
+    async for task in get_tasks():
+        text += f"• `{task['_id']}` | {task['name']} | **{task['status']}**\n"
+
+    await message.reply(text if text.strip() else "❌ No active tasks")
