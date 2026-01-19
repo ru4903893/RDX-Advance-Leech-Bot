05c496d4f2e9d4c7b7e0bce73132d52ef2f108f9
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:11:50 +0400

    Update utils.py

--- bot/helpers/utils.py
+++ bot/helpers/utils.py
@@ -1,1 +1,8 @@
+import os
+import importlib
 
+async def load_modules(app):
+    for file in os.listdir("bot/modules"):
+        if file.endswith(".py"):
+            module = f"bot.modules.{file[:-3]}"
+            importlib.import_module(module)
