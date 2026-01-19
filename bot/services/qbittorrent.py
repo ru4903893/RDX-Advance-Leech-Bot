c0fd4b9c5249ffe548b4b67f8d7c1f290ac72a69
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:17:23 +0400

    Update qbittorrent.py

--- bot/services/qbittorrent.py
+++ bot/services/qbittorrent.py
@@ -1,1 +1,12 @@
+import os
+import subprocess
+
+def start_qb():
+    subprocess.Popen([
+        "qbittorrent-nox",
+        "--webui-port=8080"
+    ])
+
+def is_available():
+    return not bool(os.getenv("DYNO"))  # Heroku check
 
