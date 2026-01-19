d9e98dd0990c7bcda4d433833645d0c6f6ab810e
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2026-01-19 14:25:34 +0400

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -1,10 +1,20 @@
 from pyrogram import filters
 from bot import app
+from bot.database.tasks_db import add_task
+import uuid
 
 @app.on_message(filters.command("mirror"))
 async def mirror(_, message):
     if len(message.command) == 1:
-        return await message.reply("❌ Send a link to mirror")
+        return await message.reply("❌ Send a direct link")
 
     link = message.text.split(maxsplit=1)[1]
-    await message.reply(f"📥 Mirroring started:\n{link}")
+    gid = str(uuid.uuid4())[:8]
+
+    await add_task(gid, message.from_user.id, "Mirror Task", "Downloading")
+
+    await message.reply(
+        f"📥 **Mirror Started**\n"
+        f"🆔 `{gid}`\n"
+        f"🔗 {link}"
+    )
